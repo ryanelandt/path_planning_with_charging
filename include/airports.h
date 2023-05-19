f4001d283f1efcc7fe0da1b4676caf894b35d56c
Ryan <ryan.elandt@gmail.com>
2023-05-19 16:27:56 -0400

    Added initial project materials

---
+++ include/airports.h
@@ -0,0 +1,20 @@
+/**********************************************/
+/******* DON'T MODIFY THIS FILE ***************/
+/**********************************************/
+#pragma once
+#include <string>
+#include <iostream>
+#include <array>
+
+struct row
+{
+    std::string name;
+    double lat;
+    double lon;
+    double rate;
+};
+
+extern std::array<row, 303> airports;
+/**********************************************/
+/******* DON'T MODIFY THIS FILE ***************/
+/**********************************************/
